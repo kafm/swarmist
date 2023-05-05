47288ea1fbf1238629c4a99ebb3ce9f9157e5d5d
Kevin Martins <kam@Air-de-Kevin.home>
2023-05-05 14:44:49 +0100

    checkpoint

--- swarmist/helpers/random.py
+++ swarmist/helpers/random.py
@@ -2,12 +2,13 @@ from typing import Union, Dict, Optional, List, TypeVar, Tuple, Callable
 from dataclasses import dataclass
 import numpy as np
 import scipy.stats as ss
-from core import Bounds 
-
-EPSILON = 1e-6
+from core import Bounds
 
 def uniform(low: float = 0.0, high: float = 1.0, size:int = None)->Union[float, List[float]]:
     return np.random.uniform(low=low, high=high, size=size)
+
+def beta(size:int = None, alpha: float = 2.0, beta: float = 2.0)->Union[float, List[float]]:
+    return  np.random.beta(alpha, beta, size=size)
 
 #scale is the expected mean
 def exponential(size:int = None, scale: float = 1.0)->Union[float, List[float]]:
@@ -17,35 +18,64 @@ def exponential(size:int = None, scale: float = 1.0)->Union[float, List[float]]:
 def rayleigh(size: int = None, scale: float = 1.0)->Union[float, List[float]]:
     return np.random.rayleigh(scale=scale,size=size)
 
-def beta(size:int = None, alpha: float = 2.0, beta: float = 2.0)->Union[float, List[float]]:
-    return  np.random.beta(alpha, beta, size=size)
+def normal(loc:float = 0.0, scale: float = 1.0, size: int = None)->Union[float, List[float]]:
+    return np.random.normal(mean=loc, sigma=scale, size=size)
 
-def normal(mean:float = 0.0, sigma: float = 1.0, size: int = None)->Union[float, List[float]]:
-    return np.random.normal(mean=mean, sigma=sigma, size=size)
+def lognormal(loc:float = 0.0, scale: float = 1.0, size: int = None)->Union[float, List[float]]:
+    return np.random.lognormal(mean=loc, sigma=scale, size=size)
 
-def lognormal(mean:float = 0.0, sigma: float = 1.0, size: int = None)->Union[float, List[float]]:
-    return np.random.lognormal(mean=mean, sigma=sigma, size=size)
+#k=shape
+def weibull(shape: float = 1.0, scale: float = 1.0, size: int = None)->Union[float, List[float]]:
+    return np.random.weibull(shape, scale=scale, size=size)
+
+#loc=location
+def cauchy(loc: float = 0.0, scale: float = 1.0, size: int = None)->Union[float, List[float]]:
+    u = np.random.uniform(size=size)
+    return loc + ( scale * np.tan(np.pi * (u-.5) ) )
+
+def levy(loc: float = 0.0, scale: float = 1.0, size: int = None)->Union[float, List[float]]:
+    u = np.random.uniform(size=size)
+    z = ss.norm.ppf(1 - u/2)
+    return loc + ( scale / ( 1/z ) **2 )
+
+def skewnormal(shape: float = 0.0, loc: float = 0.0, scale: float = 1.0, size: int = None)->Union[float, List[float]]:
+    return ss.skewnorm.rvs(a=shape, loc=loc, scale=scale, size=size)
 
 @dataclass(frozen=True)
 class BondedRandom(Bounds):
     def uniform(self, size: int = None)->Union[float, List[float]]:
         return uniform(low=self.min, high=self.max, size=size)
     
-    def beta(self, size:int = None, alpha: float = 2.0, beta: float = 2.0)->Union[float, List[float]]:
+    def beta(self, size: int = None, alpha: float = 2.0, beta: float = 2.0)->Union[float, List[float]]:
         return beta(alpha, beta, size=size) * (self.max - self.min) + self.min
     
-    #Using inverse transform sampling. 
-    #Taken from  https://stackoverflow.com/questions/25141250/how-to-truncate-a-numpy-scipy-exponential-distribution-in-an-efficient-way
-    def exponential(self, size:int = None, scale: float = None)->Union[float, List[float]]:
-        if not scale:
-            np.clip(exponential(size=size, scale=.5),0,1) * (self.max - self.min) + self.min
-        return np.clip(exponential(size=size, scale=scale),self.min,self.max)
-        # rnd_cdf = np.random.uniform(
-        #     low=ss.expon.cdf(x=self.min, scale=scale),
-        #     high=ss.expon.cdf(x=self.max, scale=scale),
-        #     size=size
-        # )
-        # return ss.expon.ppf(q=rnd_cdf, scale=scale)
+    def exponential(self, size: int = None, scale: float = 1.0)->Union[float, List[float]]:
+        exp_val = np.clip(np.divide(-np.log(np.random.uniform(size=size)),scale),0,1)
+        return self.min + exp_val * (self.max - self.min)
     
     def rayleigh(self, size: int = None, scale: float = 1.0)->Union[float, List[float]]:
-        return np.clip(rayleigh(size=size, scale=scale),0,1) * (self.max - self.min) + self.min
+        ray_val = np.clip(np.multiply(scale, np.sqrt(-2*np.log(np.random.uniform(size=size)))),0,1)
+        return self.min + ray_val * (self.max - self.min)
+    
+    def weibull(self, size: int = None, k: float = 1.0, scale: float = 1.0)->Union[float, List[float]]:
+        wei_val = np.clip(np.random.weibull(k, scale=scale, size=size),0,1)
+        return self.min + wei_val * (self.max - self.min)
+    
+    def cauchy(self, size: int = None, loc: float = 0.0, scale: float = 1.0)->Union[float, List[float]]:
+        c_val = np.clip(cauchy(loc=loc, scale=scale, size=size), 0, 1)
+        return self.min + c_val * (self.max - self.min)
+    
+    def levy(self, size: int = None, loc: float = 0.0, scale: float = 1.0)->Union[float, List[float]]:
+        l_val = np.clip(levy(loc=loc, scale=scale, size=size), 0, 1)
+        return self.min + l_val * (self.max - self.min)
+    
+    def normal(self, loc:float = 0.0, scale: float = 1.0, size: int = None)->Union[float, List[float]]:
+        return np.clip(normal(mean=loc, sigma=scale, size=size), self.min, self.max)
+
+    def lognormal(self, loc:float = 0.0, scale: float = 1.0, size: int = None)->Union[float, List[float]]:
+        return np.clip(lognormal(mean=loc, sigma=scale, size=size), self.min, self.max)
+    
+    def skewnormal(self, size: int, shape: float = 0.0, loc: float = 0.0, scale: float = 1.0)->Union[float, List[float]]:
+        return np.clip(skewnormal(shape=shape, mean=loc, sigma=scale, size=size), self.min, self.max)
+    
+    
--- swarmist/playground.py
+++ swarmist/playground.py
@@ -1,4 +1,5 @@
 #from helpers.random import BondedRandom, rayleigh, exponential
+import time
 import numpy as np
 import scipy.stats as ss
 import matplotlib.pyplot as plt
@@ -13,22 +14,62 @@ import matplotlib.pyplot as plt
 #     if rand > 10:
 #         print(rand)
 
+def gen_truncexpon(scale, lower_bound=0.0, upper_bound=1.0, size=1):
+    return ss.truncexpon(1,scale=scale, size=size)
+
+def generate_exponential0(scale, lower_bound=0.0, upper_bound=1.0, size=1):
+    exp_val = ss.expon.rvs(scale=scale, size=size)#np.clip(np.random.exponential(scale=scale, size=size),0,1)
+    return lower_bound + exp_val * (upper_bound - lower_bound)
+
+def generate_exponential_log(scale, lower_bound=0.0, upper_bound=1.0, size=1):
+    exp_val = np.divide(-np.log(np.random.uniform(size=size)),scale)
+    return lower_bound + exp_val * (upper_bound - lower_bound)
+
 def generate_exponential(scale, lower_bound=0.0, upper_bound=1.0, size=1):
     # Calculate the cumulative distribution function (CDF) of the exponential distribution
     cdf = ss.expon.cdf(upper_bound - lower_bound, scale=scale)
-    
     # Generate uniform-distributed random variables between 0 and 1
+    print(f"cdf={cdf}")
     sample_unit = np.random.uniform(0, cdf, size)
-    
     # Calculate the inverse of the CDF for the exponential distribution
     inv_cdf = ss.expon.ppf(sample_unit, scale=scale)
-    
     # Scale the samples to the range between the lower and upper bounds
     sample = lower_bound + inv_cdf
     
     return sample
-print(ss.expon.cdf(10, scale=10))
+
+def trunc_exp_rv(scale, lower_bound=0.0, upper_bound=1.0, size=1):
+    rnd_cdf = np.random.uniform(ss.expon.cdf(x=lower_bound, scale=scale),
+                                ss.expon.cdf(x=upper_bound, scale=scale),
+                                size=size)
+    print(f"rnd_cdf={rnd_cdf}")
+    return ss.expon.ppf(q=rnd_cdf, scale=scale)
+
+#print(ss.expon.cdf(10, scale=1))
+start_time = time.time()
+print("============INVERSE TRANSFORMATION SAMPLING===================")
 print(generate_exponential(scale=1.0, lower_bound=10.0, upper_bound=20.0, size=10))
+print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+print("============INVERSE TRANSFORMATION SAMPLING(trunc_exp_rv)===================")
+print(trunc_exp_rv(scale=1.0, lower_bound=10.0, upper_bound=20.0, size=10))
+print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+print("============CANONICAL=========================================")
+print(generate_exponential0(scale=1.0, lower_bound=10.0, upper_bound=20.0, size=10))
+print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+# print("============truncexpon=========================================")
+# #print(gen_truncexpon(scale=1.0, lower_bound=10.0, upper_bound=20.0, size=10))
+# print(ss.truncexpon.rvs(1, size=10))
+print("============INVERSE TRANSFORMATION SAMPLING (LOG)===================")
+print(generate_exponential_log(scale=1.0, lower_bound=10, upper_bound=20, size=10))
+print("--- %s seconds ---" % (time.time() - start_time))
+print("===========NUMPY EXP===================")
+print(np.random.exponential(scale=1.0, size=10))
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
 
 
 # Lambda = 2.5 #expected mean of exponential distribution is lambda in Scipy's parameterization
